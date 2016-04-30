2879c3550838896cff04d8b1c3641df873c98a69
Daniel Zuluaga <dzszuluaga@gmail.com>
2016-04-30 00:20:26 -0500

    !

--- RProgramming/Week 2/Programming Assignment 1/complete.R
+++ RProgramming/Week 2/Programming Assignment 1/complete.R
@@ -5,6 +5,8 @@
 # the number of complete cases. A prototype of this function follows
 
 complete <- function(directory, id = 1:332) {
+    
+    count_complete <- function(fname)  { sum(complete.cases(read.csv(fname))) }
     fnames <- list.files(directory, full.names=TRUE)[id]
     data.frame(id = id, complete = unlist(lapply(fnames, count_complete)))
 }
