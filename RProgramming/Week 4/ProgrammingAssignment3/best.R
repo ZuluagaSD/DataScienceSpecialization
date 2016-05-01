454f563a90b7a1ccb2e6ab720425002d897cbe98
Daniel Zuluaga <dzs_zuluaga@hotmail.com>
2016-05-01 10:25:16 -0500

    best Function
    Finding the best hospital in a state.

--- RProgramming/Week 4/ProgrammingAssignment3/best.R
+++ RProgramming/Week 4/ProgrammingAssignment3/best.R
@@ -6,18 +6,49 @@ dataset_url <- paste("https://d396qusza40orc.cloudfront.net/rprog%2Fdata",
 download.file(dataset_url, "2FProgAssignment3-data.zip")
 unzip("2FProgAssignment3-data.zip", exdir = "ProgAssignment3Files")
 
-outcome <- read.csv("ProgAssignment3Files/outcome-of-care-measures.csv",
+outcomeData <- read.csv("ProgAssignment3Files/outcome-of-care-measures.csv",
                     colClasses = "character")
-head(outcome)
+hospData <- read.csv("ProgAssignment3Files/hospital-data.csv",
+                     colClasses = "character")
 
 ## Histogram of the 30-day death rates from heart attack
 
-outcome[, 11] <- as.numeric(outcome[, 11])
-hist(outcome[, 11])
+outcomeData[, 11] <- as.numeric(outcomeData[, 11])
+hist(outcomeData[, 11])
 
 best <- function(state, outcome) {
     ## Read outcome data
     ## Check that state and outcome are valid
+    
+    outCheck <- match(outcome, c("heart attack", "heart failure",
+                                        "pneumonia"))
+    
+    if (is.na(outCheck)) {
+        stop(print("invalid outcome"))
+    }
+    
+    stateCounter <- dim(subset(outcomeData, outcomeData$"State" == state,
+                               select = "State"))
+    
+    if (stateCounter[1] == 0) {
+        stop(print("invalid state"))
+    }
+    
     ## Return hospital name in that state with lowest 30-day death
+    
+    if (tolower(outcome) == "heart attack") {
+        qtype <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
+    } else if (tolower(outcome) == "heart failure") {
+        qtype <- "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
+    } else if (tolower(outcome) == "pneumonia") {
+        qtype <- "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+    }
+    
+    tmpData <- subset(outcomeData, outcomeData$State == "TX",
+                      select = c("Hospital.Name", qtype))
+    head(tmpData)
+    
     ## rate
 }
+
+best("TX", "heart failure")
