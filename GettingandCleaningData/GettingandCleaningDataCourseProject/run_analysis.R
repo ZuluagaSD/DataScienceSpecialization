e11ce7f2f73338a97f7f53faabdae3e18f8cf078
ZuluagaSD <dzs_zuluaga@hotmail.com>
2016-05-05 20:38:28 -0500

    Starting!

--- GettingandCleaningData/GettingandCleaningDataCourseProject/run_analysis.R
+++ GettingandCleaningData/GettingandCleaningDataCourseProject/run_analysis.R
@@ -1,4 +1,8 @@
 
+## Load all the libraries needed
+
+library("dplyr")
+
 ## Download and unzip the Data Set into HARUSDataSet folder
 dataset_url <- paste("https://d396qusza40orc.cloudfront.net/",
                      "getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
@@ -11,3 +15,19 @@ if (!file.exists("getdata-projectfiles-UCI HAR Dataset.zip")) {
 
 ## Extract everything into the "UCI HAR Dataset" directory
 unzip("getdata-projectfiles-UCI HAR Dataset.zip")
+
+## Read files
+## Start reading features files to assign the column names
+
+tmpfeatures <- read.table("UCI HAR Dataset/features.txt")
+features <- as.vector(tmpfeatures[, 2])
+
+## Start fixing the test data set
+
+xtest <- tbl_df(read.table("UCI HAR Dataset/test/X_test.txt"))
+subjtest <- tbl_df(read.table("UCI HAR Dataset/test/subject_test.txt"))
+activity <- tbl_df(read.table("UCI HAR Dataset/test/y_test.txt"))
+
+str(full_join(subjtest, activity))
+mutate(subjtest)
+colnames(xtest) <- as.vector(features)
